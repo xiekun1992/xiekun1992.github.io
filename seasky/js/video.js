14806601e42eafb0715cb7c342445e4d78691df6
xiekun <xiekun@f3dt.com>
2017-09-15 11:16:02 +0800

    修改微信视频播放

--- seasky/js/video.js
+++ seasky/js/video.js
@@ -7,12 +7,11 @@
 	var li = document.createElement('li'),
 		a = document.createElement('a'),
 		video = document.createElement('video'),
-		videoContainer = document.createElement('div')
-		// ,
-		// videoClose = document.createElement('a');
+		videoContainer = document.createElement('div'),
+		videoClose = document.createElement('a');
 
 	a.setAttribute('href', 'javascript:void(0)');
-	// videoClose.setAttribute('href', 'javascript:void(0)');
+	videoClose.setAttribute('href', 'javascript:void(0)');
 	a.innerText = config.name;
 	video.src = config.src;
 	video.controls = true;
@@ -23,23 +22,15 @@
 	video.setAttribute('x5-video-player-type', 'h5');
 	videoContainer.style.cssText = 'position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: #000; z-index: 999; display: none;';
 	video.style.cssText = 'width: 100%; height: 100%;';
-	// videoClose.innerHTML = '&times;';
-	// videoClose.style.cssText = 'color: rgb(255, 255, 255); position: absolute; top: 0; left: 0; font-size: 0.8rem; line-height: 0.8;';
+	videoClose.innerHTML = '&times;';
+	videoClose.style.cssText = 'color: rgb(255, 255, 255); position: absolute; top: 0; right: 0.1rem; font-size: 0.8rem; line-height: 0.8;';
 	a.onclick = function(){
-		history.pushState({
-			title: 'video play'
-		}, 'video play', 'video.html');
-		playVideo();
-	}
-
-	function playVideo(){
 		_x.event.trigger('audio.stop');
 		video.src = config.src;
 		video.play();
 		videoContainer.style.display = 'block';
 	}
-	// videoClose.onclick = 
-	function stopVideo(){
+	videoClose.onclick = function(){
 		video.pause();
 		videoContainer.style.display = 'none';
 		_x.event.trigger('audio.playifneed');
@@ -47,16 +38,8 @@
 	li.appendChild(a);
 	modelNav.appendChild(li);
 	videoContainer.appendChild(video);
-	// videoContainer.appendChild(videoClose);
+	videoContainer.appendChild(videoClose);
 	document.body.appendChild(videoContainer);
-
-	window.addEventListener('popstate', function(){
-		if(history.state && history.state.title === 'video play'){
-			playVideo();
-		}else{
-			stopVideo();
-		}
-	});
 
 	// modelNav.style.left = 'calc(50% - ' + (modelNav.clientWidth/2) + 'px)';
 });
